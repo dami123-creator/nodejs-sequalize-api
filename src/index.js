0c0a17e86586da942667f7525021903d3d5b7dda
Damian Figueroa <damy.mtl@gmail.com>
2023-07-11 19:03:23 -0300

    feat: sequalize schemas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import app from "./app.js";
 import {sequelize} from './database/database.js'
 
+import "./models/Project.js";
+import "./models/Task.js";
+
 async function main () {
     try {
-        await sequelize.authenticate();
+        await sequelize.sync();
         console.log('Connection has been established succesfully.')
         app.listen(4000)
         console.log('Server is listening on port', 4000)
