a59692a706cc4b6056bfae33425e966d844fdb89
Damian Figueroa <damy.mtl@gmail.com>
2023-07-12 00:28:38 -0300

    feat: updated task controllers

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -26,52 +26,44 @@ export async function getTasks(req, res) {
   }
 }
 
-export async function updateTask(req, res) {
-  const { id } = req.params;
-  // const { projectid, name, done } = req.body;
-  try {
-    // const updatedTask = await Task.update(
-    //   { name, done, projectid },
-    //   { where: { id } }
-    // );
-
-    const task = await Task.findOne({
-      attributes: ["name", "projectId", "done", "id"],
-      where: { id },
-    });
-
-    task.set(req.body);
-
-    await task.save();
-
-    res.json(task);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-}
-
-export async function deleteTask(req, res) {
-  const { id } = req.params;
-  try {
-    await Task.destroy({
-      where: { id },
-    });
-
-    return res.sendStatus(204);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-}
-
 export async function getTask(req, res) {
-  const { id } = req.params;
-  try {
+  const {id} = req.params
+  try{
     const task = await Task.findOne({
-      where: { id },
-      attributes: ["id", "projectId", "name", "done"],
-    });
-    res.json(task);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
+      where:{id},
+    })
+
+  }catch(error){
+    return res.status(500).json({message: error.message})
   }
+
+}
+
+export async function deleteTask(req, res){
+  try {
+    const {id} = req.params
+    const result = Task.destroy({
+      where : { id },
+    })
+  return res.status(204)
+  } catch (error) {
+    return res.status(500).json({message: error.message})
+    
+  }
+  
+}
+
+export async function updateTask(req, res){
+  try {
+    const {id} = req.params
+    const task = Task.findOne({
+      where:{id},
+    })
+    task.set(req.body)
+    await task.save()
+    return res.json(task)
+  } catch (error) {
+    return res.status(500).json({message: error.message})
+  }
+ 
 }
